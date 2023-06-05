6c6d978d8632f403f891d6fef70b783e2ec55fea
Qiming01 <52878819+Qiming01@users.noreply.github.com>
2023-06-05 14:00:12 +0800

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Des des = new Des(2020216774);
+        String inputFile = "/Users/qm/Encryption/test/测试图片.png";
+        String encryptedFile = inputFile.replace(".", "_des_en.");
+        String decryptedFile = inputFile.replace(".", "_des_de.");
+        des.encryptFile(inputFile, encryptedFile);
+        des.decryptFile(encryptedFile, decryptedFile);
+    }
+}
